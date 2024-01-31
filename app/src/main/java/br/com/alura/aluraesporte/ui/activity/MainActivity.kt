9383561118febbff59f5c4338611f35a1b277c90
raelrzd <israelrezendeluiz2903@gmail.com>
2024-01-31 15:13:12 -0300

    Adicionado listener para aplicar configurações quando houver eventos de navegação.

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -2,13 +2,21 @@ package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy { findNavController(R.id.main_activity_nav_host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        navController.addOnDestinationChangedListener { navController, navDestination, bundle ->
+            title = navDestination.label
+        }
+
     }
 
 }
