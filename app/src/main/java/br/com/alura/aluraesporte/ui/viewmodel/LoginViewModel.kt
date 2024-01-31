eab259763f61fb15f6d2b8dc87ef35e7cbb0b65e
raelrzd <israelrezendeluiz2903@gmail.com>
2024-01-31 10:51:09 -0300

    Adicionada tela de login com navegação condicional na reabertura do App usando SharedPreferences.

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.alura.aluraesporte.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import br.com.alura.aluraesporte.repository.LoginRepository
+
+class LoginViewModel(private val repository: LoginRepository) : ViewModel() {
+
+    fun loga() = repository.loga()
+
+    fun estaLogado() = repository.estaLogado()
+
+}
