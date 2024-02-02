42007ceba982a694d65d426dc91aaf455721df42
raelrzd <israelrezendeluiz2903@gmail.com>
2024-02-02 12:15:15 -0300

    Adicionado Bottom Navigation View para navegação entre lista de proutos e lista pagamentos.

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
+import kotlinx.android.synthetic.main.main_activity.main_activity_bottom_navigation
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -26,9 +28,9 @@ class MainActivity : AppCompatActivity() {
                     else supportActionBar?.hide()
                 }
             })
-
-
         }
+
+        main_activity_bottom_navigation.setupWithNavController(navController)
 
     }
 
